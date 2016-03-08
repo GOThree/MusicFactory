a953e402bb060845f73ae4133f950e439e42940c
Vladimir Orelov <vladimirorelov@gmail.com>
2016-03-08 20:12:22 +0100

    using the JwtStrategy for some routes #12

--- server/routes.js
+++ server/routes.js
@@ -2,28 +2,45 @@ var passport = require('passport');
 var Account = require('./models/account');
 var express = require('express');
 var router = express.Router();
-
+var passportService = require('./services/passportService');
 
 router.post('/register', function(req, res, next){
     Account.register(new Account({username: req.body.username}), req.body.password, function(err){
         if(err){
             console.log('error while user register', err);
+        } else {
+            // issue token
+            var token = passportService.createToken(req.user);
+            // return token
+            res.send(token);
         }
-        console.log('user registered!');
-    })
+    });
 });
 
-router.get('/', function (req, res, next) {
-  console.log('Request Type:', req.method);
-  res.send('Hello World!')
-  next();
-});
+router.get('/',
+    function(req, res) {
+        res.send('Hello World!');
+    }
+);
 
-router.post('/login', passport.authenticate('local'), function(req, res) {
-  console.log('User: ' + req.user)
+router.get('/jwt_test_route', passport.authenticate('jwt', { session: false}),
+    function(req, res) {
+        // should not s
+        res.send(req.user.id);
+    }
+);
+
+router.post('/login', passport.authenticate('local'), function(req, res) {  
+    // issue token
+    var token = passportService.createToken(req.user);
+    // return token
+    res.send(token);
 });
 
 router.get('/logout', function(req, res) {
+  // the jwt should be deleted from the FE
+  // req.logout() is not needed because the LocalStrategy 
+  // is not using session
   req.logout();
   res.redirect('/');
 });
