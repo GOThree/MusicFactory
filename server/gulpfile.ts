32f980104f8d982c451409f436a627bee50fc02c
ivoPashov <ivopashov@abv.bg>
2016-03-19 23:06:15 +0200

    include systemjs as a dependency in order to get modules loading

--- server/gulpfile.ts
+++ server/gulpfile.ts
@@ -1,6 +1,10 @@
-/// <reference path="typings/main.d.ts" />
 
 import * as gulp from 'gulp';
 import {runSequence, task} from './tools/utils';
 
+//debugger;
 gulp.task('clean', done => task('clean')(done));
+
+//gulp.task('build', function() {
+//  console.log('Gulp is running!')
+//});
