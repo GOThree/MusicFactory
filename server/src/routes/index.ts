035984af1dd6f1fb048218bd23fd8eb2c40513c0
Vladimir Orelov <vladimirorelov@gmail.com>
2016-03-22 17:21:22 +0100

    added Authentication routes (register/login)

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,9 @@
 import {TodoRoutes} from '../components/todo/routes/todo-routes';
-
+import {AuthenticationRoutes} from '../components/authentication/routes/authentication-routes';
 export class Routes {
   static init(app: any, router: any) {
     TodoRoutes.init(router);
+    AuthenticationRoutes.init(router);
 
     app.use('/', router);
   }
