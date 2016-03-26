7233ba44c0b8a7e05a78f10af9bf8e738968be50
ivoPashov <ivopashov@abv.bg>
2016-03-26 18:27:02 +0200

    moved authentication configuration in a separate file

--- server/src/config/routes.config.ts
+++ server/src/config/routes.config.ts
@@ -3,19 +3,24 @@ import * as helmet from 'helmet';
 import {AuthenticationRoutes} from '../components/authentication/routes/authentication-routes';
 import {TodoRoutes} from '../components/todo/routes/todo-routes';
 import * as passport from 'passport';
+import * as express from 'express';
 
 export class RoutesConfig {
-    static init(application:any, exp:any):void {
+    static init(application:any):void {
         let _root = process.cwd();
-        let router = exp.Router();
+        let router = express.Router();
 
-        application.use(exp.static(_root));
+        application.use(express.static(_root));
         application.use(bodyParser.json());
+        application.use(bodyParser.urlencoded({
+            extended: true
+        }));
         application.use(helmet());
+
         AuthenticationRoutes.init(router);
-        router.use(passport.authenticate('jwt', { session: false}));
         TodoRoutes.init(router);
 
+        router.use(passport.authenticate('jwt', { session: false}));
         application.use('/', router);
     }
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -6,52 +6,18 @@ import * as express from 'express';
 import * as os from 'os';
 import * as cors from 'cors';
 import * as morgan from 'morgan';
-import * as bodyParser from 'body-parser';
-import * as passport from 'passport';
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-
 import {RoutesConfig} from './config/routes.config';
+import {AuthConfig} from './config/auth.config';
 import {DBConfig} from './config/db.config';
-import Account from './components/authentication/dao/authentication-dao';
-import {JwtStrategyConfig} from './components/authentication/config/jwt-strategy-config';
 import logger from './config/logger.config';
 
 const app = express();
-app.listen(PORT);
+
+RoutesConfig.init(app);
+AuthConfig.init(app);
+DBConfig.init();
+
 app.use(cors());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-
-// Authentication
-app.use(passport.initialize());
-passport.use(Account.createStrategy());
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
-
-// jwt token authentication
-JwtStrategyConfig.jwtFromRequest = ExtractJwt.fromAuthHeader();
-passport.use(new JwtStrategy(JwtStrategyConfig, function(jwt_payload, done) {
-    // get user ID
-    if(!jwt_payload.uid) {
-        return done('Missing uid in payload', false);
-    }
-    var userId = jwt_payload.uid;
-
-    // find the user by ID
-    Account.findById(userId, function(err, user) {
-        if (err) {
-            return done(err, false);
-        }
-        if (user) {
-            done(null, user);
-        } else {
-            done(null, false);
-        }
-    });
-}));
 
 // Logging
 var winstonStream = {
@@ -62,8 +28,8 @@ var winstonStream = {
 
 app.use(morgan('dev', { stream: winstonStream }));
 
-DBConfig.init();
-RoutesConfig.init(app, express);
+app.listen(PORT, function(){
+    logger.debug(`up and running @: ${os.hostname()} on port: ${PORT}`);
+    logger.debug(`enviroment: ${process.env.NODE_ENV || 'development'}`);
+});
 
-logger.debug(`up and running @: ${os.hostname()} on port: ${PORT}`);
-logger.debug(`enviroment: ${process.env.NODE_ENV}`);
